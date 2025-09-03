b6577a8222c1f7a75746f6d7c437de8276fb0f4b
Irene Wang <wangirene2008@gmail.com>
2025-09-03 00:58:34 -0400

    dashboard hero

---
+++ app/Navbar.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { useState } from 'react';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoMdClose } from "react-icons/io";
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const navItems = ['About', 'Sponsors', 'Team', 'FAQ', 'Schedule'];
+
+    const scrollToSection = (id:string) => {
+        const section = document.getElementById(id);
+        setIsOpen(false)
+        if (section) {
+          section.scrollIntoView({ behavior: 'smooth' });
+        }
+      }
+      
+  return (
+    <div>
+        <header className="w-full flex justify-end mb-30 z-9999 ">
+        <nav className="space-x-2">
+          <div className="hidden md:flex">
+            {navItems.map((item) => (
+              <button
+                key={item}
+                className="bg-gradient-to-r from-pink-300 to-sky-600 text-white px-8 py-4 mx-2 rounded-lg font-semibold text-[20px] hover:drop-shadow-[0_0_5px_rgba(255,255,255)] hover:ring-1 linear duration-200"
+                onClick={() => {scrollToSection(item)}}
+              >
+                {item}
+              </button>
+            ))}
+          </div>
+          {/* Mobile Menu */}
+          <div className="md:hidden">
+            <button onClick={() => setIsOpen(!isOpen)}>
+              {isOpen ? <IoMdClose size={36} color="white"/> : <GiHamburgerMenu size={36} color="white"/>}
+            </button>
+          </div>
+
+          {isOpen && (
+            <div className="absolute top-8 right-20 z-[9999] flex flex-col items-center md:hidden gap-4 bg-black/50 p-8">
+              {navItems.map((item) => (
+                <button
+                key={item}
+                className=" z-[9999] w-full bg-gradient-to-r from-pink-300 to-sky-600 text-white px-8 py-4 mx-2 rounded-lg font-semibold text-[20px] hover:drop-shadow-[0_0_5px_rgba(255,255,255)] hover:ring-1 linear duration-200"
+                onClick={() => {scrollToSection(item)}}
+              > {item} </button>
+              ))}
+            </div>
+          )}
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+export default Navbar
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,33 @@
+"use client";
+
 import React from "react"
+import Image from "next/image"
 
 export default function Dashboard() {
-    return <div>
-        
-    </div>
+    return (
+        <div className="min-h-screen bg-black text-white flex flex-col items-center p-4 mt-30">
+            {/* Headphones */}
+            <Image
+            src="/headphones.png"
+            alt="RythmHacks Headphones"
+            width={200}
+            height={200}
+            className="absolute top-8 left-8 z-20 hidden lg:block"
+            />
+    
+            {/* Hero Section */}
+            <main className="flex flex-col items-center justify-center z-10">
+            <div className="text-center mb-20">
+                <h1 className="text-8xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-pink-300 to-sky-600 ">
+                RythmHacks
+                </h1>
+                <p className="mt-2 text-gray-400 text-3xl ">Experience the magic of tech</p>
+
+                <h3 className="text-6xl font-bold m-20 text-white drop-shadow-[0_0_20px_rgb(255,255,255)]" > HACKER DASHBOARD </h3>
+            </div>
+    
+            </main>
+        </div>
+        )
 
 }
